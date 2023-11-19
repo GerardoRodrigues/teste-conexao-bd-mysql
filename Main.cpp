f1c3ef63268f5b956dbd3e16a24d67b158a3e72f
Gerardo Rodrigues <gerardofilho56@gmail.com>
2023-11-19 20:40:15 -0300

    Estabelecendo conexao com o banco de dados criado

--- Main.cpp
+++ Main.cpp
@@ -3,8 +3,22 @@
 
 int main(){
     MYSQL *connect = mysql_init(NULL);
-    !connect ? std::cerr << "MySQL n foi conectado" : std::cout << "MySQL conectado com sucesso";
+    
+    if(!connect){
+        std::cout << "MySQL não foi inicializado pelo sistema de inicialização.\n";
+        return 1;
+    }
 
+    connect = mysql_real_connect(connect, "localhost", "root", "", "cpp", 0, NULL, 0);
+
+    if(connect){
+        std::cout << "Conectado com sucesso ao MySQL!\n";
+    }else{
+        std::cout << "Falha ao conectar ao banco de dados!\n";
+    }
+    
     mysql_close(connect);
+    
     return 0;
 }
+// g++ Main.cpp -L'C:\Program Files\MySQL\MySQL Server 8.0\lib' -I'C:\Program Files\MySQL\MySQL Server 8.0\include' -lmysql
